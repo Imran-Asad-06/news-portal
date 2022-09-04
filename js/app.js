6a2b083983af7e4636be62eb08fbd2d929f8174b
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-09-04 09:25:42 +0600

    add modal

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const loadContainer = () =>{
 }
 
 const disPlayContainer = data => {
+    
     const navContainer = document.getElementById('nav-container');
     for(const nav of data.data.news_category){
         const newDiv = document.createElement('div')
@@ -23,10 +24,12 @@ const loadNews =id=>{
     .then(newsData => displayNews(newsData))
     
 }
+
 const displayNews =newsData =>{
+    
     const showNews = document.getElementById('show-news')
     showNews.textContent = ''
-   for(const allNews of newsData.data){
+    for(const allNews of newsData.data){
     const newsDiv = document.createElement('div')
       newsDiv.classList.add('news-item')
       newsDiv.innerHTML = `
@@ -39,15 +42,46 @@ const displayNews =newsData =>{
           <div class="card-body">
             <h5 class="card-title">${allNews.title}</h5>
             <p class="card-text">${allNews.details.slice(0, 600)+ "......."}</p>
-            <p class="card-text"><small class="text-muted">${allNews.total_view}</small></p>
-          </div>
+            <div class ="d-flex justify-content-around">
+            <div>   <img id ="authorImg" class="rounded" src ="${allNews.author.img}"> <small class="text-muted">
+            <p class="card-text"> ${allNews.author.name}</small></p>  </div>
+           <p class="card-text> <small class="text-muted"> Views : ${allNews.total_view }</small></p>
+            <small><button id ="${allNews._id}" onclick="showDetails('${allNews._id}')"  type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Details</button></small>
+           
+               <div>
+            </div>
         </div>
       </div>
     </div>
       `
     showNews.appendChild(newsDiv)
 }
+ toggleSpinner(false);
+}
+// Spinner
+const toggleSpinner = isLoading => {
+  const loaderSection = document.getElementById('loader');
+  if(isLoading){
+      loaderSection.classList.remove('d-none')
+  }
+  else{
+      loaderSection.classList.add('d-none');
+  }
 }
 
-
+const showDetails = newsId => {
+  fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
+  .then(res => res.json())
+  .then(detailsdata => modalDetails(detailsdata)  )
+}
+const modalDetails = detailsdata =>{
+  //console.log(detailsdata.data[0].author.name)
+  const modalTitle = document.getElementById('exampleModalLabel')
+  modalTitle.innerText = detailsdata.data[0].author.name? detailsdata.data[0].author.name:'No Name Found'
+  const modalBody = document.getElementById('modalbody')
+  modalBody.innerHTML =`
+    <p> Total Views : ${detailsdata.data[0].total_view? detailsdata.data[0].total_view:'No Data Found'} </P>
+    <p> Rating : ${detailsdata.data[0].rating.number? detailsdata.data[0].rating.number:'No Data Found'} </P>
+  `
+}
 loadContainer()
